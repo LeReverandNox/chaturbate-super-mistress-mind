f5e85432a241f465e60421a72860193e24e1a4c3
lrn <lrn@gmail.com>
2019-04-11 00:44:21 +0200

    Lint App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import SuperMisstressmind from './SuperMistressmind.js';
+import SuperMisstressmind from './SuperMistressmind';
 
 export default class App {
   constructor(cb, cbjs, commandParser) {
@@ -10,12 +10,13 @@ export default class App {
 
     this._initListeners();
   }
+
   _initListeners() {
-    this.cb.onMessage((msg) => {
+    this.cb.onMessage(msg => {
       return this.onMessage(msg);
     });
 
-    this.cb.onTip((tip) => {
+    this.cb.onTip(tip => {
       return this.onTip(tip);
     });
   }
@@ -25,35 +26,37 @@ export default class App {
   }
 
   onMessage(msgObj) {
-    let msg = msgObj["m"];
+    const msg = msgObj.m;
 
     if (this._isCommand(msg)) {
-      msgObj["X-Spam"] = true;
+      msgObj['X-Spam'] = true;
       this._handleCommand(msgObj);
     }
 
     return msgObj;
   }
+
   _isCommand(str) {
     return str.startsWith(config.CMD_PREFIX);
   }
+
   _handleCommand(msgObj) {
-    let user = msgObj["user"];
-    let msg = msgObj["m"];
-    let args = this.commandParser.parse(msg);
-    let cmd = args[0] || "";
-    let responseText;
+    const { user } = msgObj.user;
+    const msg = msgObj.m;
+    const args = this.commandParser.parse(msg);
+    const cmd = args[0] || '';
 
     if (this._isValidCommand(cmd)) {
-      let response = this.commands[cmd].handler(user, args);
+      const response = this.commands[cmd].handler(user, args);
       this._sendResponse(response);
     } else {
-      this._sendResponse({user, content: [{txt: "Invalid command."}]});
+      this._sendResponse({ user, content: [{ txt: 'Invalid command.' }] });
     }
   }
 
-  onTip(tip){
-    this.cb.log("Un tip");
+  onTip(tip) {
+    this.cb.log('Un tip');
+    this.cb.log(tip);
   }
 
   _isValidCommand(name) {
@@ -61,51 +64,59 @@ export default class App {
   }
 
   _sendResponse(response) {
-    let user = response.user || "";
-    let group = response.group || "";
+    const user = response.user || '';
+    const group = response.group || '';
 
-    for (let line of response.content) {
-      let bg = line.bg || "";
-      let fg = line.fg || "";
-      let weight = line.weight || "";
-      let txt = line.txt || "";
+    response.content.forEach(line => {
+      const bg = line.bg || '';
+      const fg = line.fg || '';
+      const weight = line.weight || '';
+      const txt = line.txt || '';
       this.cb.sendNotice(txt, user, bg, fg, weight, group);
-    }
+    });
   }
 
   get commands() {
     return {
-      "helpFr": {
+      helpFr: {
         modelOnly: false,
         desc: {
           short: {
-            fr: [`${config.CMD_PREFIX}helpFr [command] - Affiche l'aide du jeu (Fr)`],
-            en: [`Show the help (En)`]
+            fr: [
+              `${
+                config.CMD_PREFIX
+              }helpFr [command] - Affiche l'aide du jeu (Fr)`,
+            ],
+            en: [`Show the help (En)`],
           },
           long: {
-            fr: [`${config.CMD_PREFIX}helpFr [command] - Affiche des informations utiles sur le jeu (Fr).`,
-                 `Si [command] est specifie, affiche l'aide de la commande.`],
-            en: [`The help command blablabla`]
-          }
+            fr: [
+              `${
+                config.CMD_PREFIX
+              }helpFr [command] - Affiche des informations utiles sur le jeu (Fr).`,
+              `Si [command] est specifie, affiche l'aide de la commande.`,
+            ],
+            en: [`The help command blablabla`],
+          },
         },
         handler: (user, args) => {
-          let content = [];
-          let cmd = args[1] || "";
+          const content = [];
+          const cmd = args[1] || '';
           if (this._isValidCommand(cmd)) {
             if (this._isModel(user) || !this.commands[cmd].modelOnly) {
-              for (let  helpLine of this.commands[cmd].desc.long.fr) {
-                content.push({txt: helpLine});
-              }
+              this.commands[cmd].desc.long.fr.forEach(helpLine => {
+                content.push({ txt: helpLine });
+              });
             } else {
-              content.push({txt: "Acces interdit"});
+              content.push({ txt: 'Acces interdit' });
             }
           } else {
-            content.push({txt: "Bienvenue dans l'aide !"});
+            content.push({ txt: "Bienvenue dans l'aide !" });
           }
 
-          return {user, content};
-        }
-      }
+          return { user, content };
+        },
+      },
     };
   }
 }
