dccf4f13cff3ef7b8a2302686f365cf74bf591b2
lrn <lrn@gmail.com>
2019-04-12 23:55:13 +0200

    Now send cmd reponses in handlers, set listeners outside App

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ export default class App {
   constructor(cb, cbjs, commandParser) {
     this._cb = cb;
     this._cbjs = cbjs;
+    this.commandParser = commandParser;
 
     this.authors = ['LeReverandNox'];
     this.version = '0.1';
 
     this._createSettingForm();
 
-    this.commandParser = commandParser;
     this._modelName = cb.room_slug;
     this._settings = this._parseSettings();
 
-    this._initListeners();
     this._run();
   }
 
@@ -51,29 +50,8 @@ export default class App {
     ];
   }
 
-  _initListeners() {
-    this._cb.onMessage(msg => {
-      return this.onMessage(msg);
-    });
-
-    this._cb.onTip(tip => {
-      return this.onTip(tip);
-    });
-  }
-
   _isModel(name) {
     return name === this._modelName;
-  }
-
-  onMessage(msgObj) {
-    const msg = msgObj.m;
-
-    if (this._isCommand(msg)) {
-      msgObj['X-Spam'] = true;
-      this._handleCommand(msgObj);
-    }
-
-    return msgObj;
   }
 
   _isCommand(str) {
@@ -88,13 +66,13 @@ export default class App {
     if (this._isValidCommand(cmd)) {
       if (this._commandAuthorized(user, cmd)) {
         try {
-          const response = this.commands[cmd].handler(user, args);
-          EventBus.emit('sendResponse', response);
+          this._commands[cmd].handler(user, args);
         } catch (error) {
           EventBus.emit('sendError', {
             to: user,
             error: new Error(
-              `${error.message} \n${this.commands[cmd].desc.short.en}`,
+              `${error.message}
+              ${this._commands[cmd].desc.short.en}`,
             ),
           });
         }
@@ -110,20 +88,31 @@ export default class App {
       });
   }
 
+  _isValidCommand(name) {
+    return name in this._commands;
+  }
+
+  _commandAuthorized(user, cmd) {
+    return this._isModel(user) ? true : !this._commands[cmd].modelOnly;
+  }
+
+  onMessage(msgObj) {
+    const msg = msgObj.m;
+
+    if (this._isCommand(msg)) {
+      msgObj['X-Spam'] = true;
+      this._handleCommand(msgObj);
+    }
+
+    return msgObj;
+  }
+
   onTip(tip) {
     this._cb.log('Un tip');
     this._cb.log(tip);
   }
 
-  _isValidCommand(name) {
-    return name in this.commands;
-  }
-
-  _commandAuthorized(user, cmd) {
-    return this._isModel(user) ? true : !this.commands[cmd].modelOnly;
-  }
-
-  get commands() {
+  get _commands() {
     return {
       helpFr: {
         modelOnly: false,
@@ -136,31 +125,29 @@ export default class App {
             en: `${Config.CMD_PREFIX}helpFr [command] - Show the help (Fn)`,
           },
           long: {
-            fr: [
-              `${
-                Config.CMD_PREFIX
-              }helpFr [command] - Affiche des informations utiles sur le jeu (Fr).`,
-              `Si [command] est specifie, affiche l'aide de la commande.`,
-            ],
-            en: [`The help command blablabla`],
+            fr: `${
+              Config.CMD_PREFIX
+            }helpFr [command] - Affiche des informations utiles sur le jeu (Fr).
+            Si [command] est specifie, affiche l'aide de la commande.`,
+            en: `The help command blablabla`,
           },
         },
         handler: (user, args) => {
           const content = [];
           const [, cmd = ''] = args;
           if (this._isValidCommand(cmd)) {
-            if (this._isModel(user) || !this.commands[cmd].modelOnly) {
-              this.commands[cmd].desc.long.fr.forEach(helpLine => {
-                content.push({ txt: helpLine });
-              });
+            if (this._isModel(user) || !this._commands[cmd].modelOnly) {
+              content.push({ txt: this._commands[cmd].desc.long.fr });
             } else {
-              content.push({ txt: 'Acces interdit' });
+              content.push({
+                txt:
+                  "You don't have the permission to see the help for this command.",
+              });
             }
           } else {
             content.push({ txt: "Bienvenue dans l'aide !" });
           }
-
-          return { user, content };
+          EventBus.emit('sendResponse', { user, content });
         },
       },
       newround: {
@@ -173,8 +160,8 @@ export default class App {
             }newround <number of colors> <code> <goal> - Starts a new round.`,
           },
           long: {
-            fr: [],
-            en: [],
+            fr: ``,
+            en: ``,
           },
         },
         handler: (user, args) => {
@@ -185,7 +172,7 @@ export default class App {
             goal = '',
           ] = args;
           const txt = this._game.newRound(nbAvailablePegs, codeStr, goal);
-          return { user, content: [{ txt }] };
+          EventBus.emit('sendResponse', { user, content: [{ txt }] });
         },
       },
       pause: {
@@ -196,13 +183,13 @@ export default class App {
             en: `${Config.CMD_PREFIX}pause - Pause / Resume the game.`,
           },
           long: {
-            fr: [],
-            en: [],
+            fr: ``,
+            en: ``,
           },
         },
         handler: (user, args) => {
           const txt = this._game.pause();
-          return { user, content: [{ txt }] };
+          EventBus.emit('sendResponse', { user, content: [{ txt }] });
         },
       },
     };
